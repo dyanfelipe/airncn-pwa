58adcc678460b2156b7a51ec2a70fcc92373c335
dyanfelipe <dyansilva@outlook.com>
2019-12-27 15:56:24 -0300

    Pages images

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../../services/api";
+import "./styles.css";
 
 export default function Dashborad() {
-  return <div />;
+  const [spots, setSpots] = useState([]);
+  useEffect(() => {
+    async function loadSpots() {
+      const userid = localStorage.getItem("user");
+      const result = await api.get("/dashboard", {
+        headers: { userid }
+      });
+      setSpots(result.data);
+    }
+    loadSpots();
+  }, []); // array vazio vai ser axec apenas uma unica vez
+  return (
+    <>
+      <ul className="spot-list">
+        {spots.map(spots => (
+          <li key={spots._id}>
+            <header
+              style={{ backgroundImage: `url(${spots.thumbnail_url})` }}
+            />
+            {console.log(spots.thumbnail_url)}
+            <strong>{spots.company}</strong>
+            <span>{spots.price ? `RR${spots.price}/dia` : "GRATUITO"}</span>
+          </li>
+        ))}
+      </ul>
+      <Link to="/new">
+        <button className="btn">Novo Spot</button>
+      </Link>
+    </>
+  );
 }
--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,76 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import camera from "../../assets/camera.svg";
+import "./styles.css";
+import api from "../../services/api";
 
-export default function New() {
-  return <div />;
+export default function New({ history }) {
+  const [thumbnail, setThumbnail] = useState(null);
+  const [company, setCompany] = useState("");
+  const [techs, setTechs] = useState("");
+  const [price, setPrice] = useState("");
+
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail]);
+
+  async function handleSubmit(event) {
+    // enviar no formato multipart
+    const data = new FormData();
+    event.preventDefault();
+    const userid = localStorage.getItem("user");
+
+    data.append("thumbnail", thumbnail);
+    data.append("company", company);
+    data.append("techs", techs);
+    data.append("price", price);
+
+    await api.post("/spots", data, {
+      headers: { userid }
+    });
+
+    history.push("/dashboard");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label
+        id="thumbnail"
+        style={{ backgroundImage: `url(${preview})` }}
+        className={thumbnail ? "han-thumbnail" : ""}
+      >
+        <input
+          type="file"
+          onChange={event => setThumbnail(event.target.files[0])}
+        />
+        <img src={camera} />
+      </label>
+      <label htmlFor="company">Empresa *</label>
+      <input
+        id="company"
+        placeholder="Nome da empresa"
+        value={company}
+        onChange={event => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="company">Tecnologias *</label>
+      <input
+        id="techs"
+        placeholder="Nome da empresa"
+        value={techs}
+        onChange={event => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="company">
+        Valor da Diária * <span>"Em branco para gratuito"</span>
+      </label>
+      <input
+        id="price"
+        placeholder="Nome da empresa"
+        value={price}
+        onChange={event => setPrice(event.target.value)}
+      />
+      <button type="submit" className="btn">
+        Cadastra
+      </button>
+    </form>
+  );
 }
